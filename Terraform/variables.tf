fef17c2bea2d39a8d680c42ad38bae2283c83555
Fares <faresraboudi308@gmail.com>
2024-11-09 13:43:08 +0100

    add microservice

--- Terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for the EC2 instances."
-  type        = string
-  default     = "cicd"
-}
-# Variables
-variable "region" {
-  default = "us-east-1"
-}
